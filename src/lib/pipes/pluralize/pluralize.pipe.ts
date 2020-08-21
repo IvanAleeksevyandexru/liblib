d1d894b923873b144b067761757b8d0263071051
gromyko <vladimir.gromyko@rtlabs.ru>
2020-08-21 11:07:04 +0300

    master up to 0.0.419

--- src/lib/pipes/pluralize/pluralize.pipe.ts
+++ src/lib/pipes/pluralize/pluralize.pipe.ts
@@ -1,4 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { HelperService } from '../../services/helper/helper.service';
 
 @Pipe({
   name: 'pluralize'
@@ -6,8 +7,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class PluralizePipe implements PipeTransform {
 
   public transform(captions: string[], count: number): string {
-    const cases = [2, 0, 1, 1, 1, 2];
-    return captions[(count % 100 > 4 && count % 100 < 20) ? 2 : cases[(count % 10 < 5) ? count % 10 : 5]];
+    return HelperService.pluralize(count, captions);
   }
 
 }
