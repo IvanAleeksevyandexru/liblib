fd2451fd0d1e2c1c2aad82f49057ff4a45e40f96
gromyko <vladimir.gromyko@rtlabs.ru>
2020-08-24 10:52:54 +0300

    EPGUCORE-32645

--- src/lib/services/health/health.service.ts
+++ src/lib/services/health/health.service.ts
@@ -79,18 +79,18 @@ export class HealthService {
     }
   }
 
-  public measureDomEvents(id) {
+  public measureDomEvents(id, eventInfo = {}) {
     // tslint:disable-next-line:deprecation
     if (window.performance && window.performance.timing) {
       // tslint:disable-next-line:deprecation
       const timingApiObj = window.performance.timing;
       const dcl = timingApiObj.domContentLoadedEventEnd - timingApiObj.navigationStart;
       const complete = timingApiObj.loadEventEnd - timingApiObj.navigationStart;
-      this.send(id, dcl, 0);
-      this.send(id, complete, 0);
+      this.send(id, dcl, 0, eventInfo);
+      this.send(id, complete, 0, eventInfo);
     } else {
       // если браузер не подерживает performance
-      this.send(id, 'NotSupportedPerfomanceBrowser', 1);
+      this.send(id, 'NotSupportedPerfomanceBrowser', 1, eventInfo);
     }
   }
 
