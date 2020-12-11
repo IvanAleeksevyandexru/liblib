5e9deb8dbbcfa5cc78ba99d65b78e02485a7b3db
natalia.tarantseva <natalia.tarantseva@rtlabs.ru>
2020-12-11 20:27:26 +0300

    EPGUCORE-42407: fix

--- src/lib/services/constants.service.ts
+++ src/lib/services/constants.service.ts
@@ -340,34 +340,7 @@ export class ConstantsService {
         mnemonic: 'partnersFeedsDraft'
       }
   ]);
-  public readonly PAYMENT_TABS = new Tabs([
-    {
-      id: 'overview',
-      name: 'TABS.OVERVIEW.TITLE',
-      url: `${this.loadService.config.lkApiUrl}overview`,
-      metric: {name: this.TABS_METRIC_NAME, action: 'overview'}
-    }, {
-      id: 'statements',
-      name: 'TABS.ORDERS.TITLE',
-      url: `${this.loadService.config.lkApiUrl}orders`,
-      metric: {name: this.TABS_METRIC_NAME, action: 'myOrders'}
-    }, {
-      id: 'profile',
-      name: 'TABS.PROFILE.TITLE',
-      url: `${this.loadService.config.lkApiUrl}profile`,
-      metric: {name: this.TABS_METRIC_NAME, action: 'documentsData'}
-    }, {
-      id: 'messages',
-      name: 'TABS.MESSAGES.TITLE',
-      url: `${this.loadService.config.lkApiUrl}messages`,
-      metric: {name: this.TABS_METRIC_NAME, action: 'messages'}
-    }, {
-      id: 'permissions',
-      name: 'TABS.PERMISSIONS.TITLE',
-      url: `${this.loadService.config.lkApiUrl}permissions`,
-      metric: {name: this.TABS_METRIC_NAME, action: 'permissions'}
-    }
-  ]);
+
   public readonly ORDERS_CATEGORIES = [
     {
       text: 'Все',
@@ -966,8 +939,4 @@ export class ConstantsService {
     'application/x-tar': 'TAR',
     'text/plain': 'TXT',
   };
-
-  constructor(
-    private loadService: LoadService
-  ) {}
 }
