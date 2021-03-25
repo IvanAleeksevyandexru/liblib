68f746d80d5e1b9f577f84b7fa4686e00d9b05e0
victoria.fedyunina <victoria.fedyunina@rtlabs.ru>
2021-03-25 08:53:29 +0300

    EPGUCORE-35201 (EPGUCORE-50388): Проверка полномочий на просмотр Единформера

--- src/lib/components/informer/informer.component.ts
+++ src/lib/components/informer/informer.component.ts
@@ -14,7 +14,6 @@ import { DeclinePipe } from '../../pipes/decline/decline.pipe';
 import { YaMetricService } from '../../services/ya-metric/ya-metric.service';
 import { LoadService } from '../../services/load/load.service';
 import { Router } from '@angular/router';
-import { ProfileService } from '../../services/profile/profile.service';
 
 @Component({
   selector: 'lib-informer',
@@ -48,16 +47,11 @@ export class InformerComponent implements OnInit {
     private yaMetricService: YaMetricService,
     private loadService: LoadService,
     private router: Router,
-    private profileService: ProfileService,
   ) {
   }
 
   public ngOnInit() {
-    if (['L', 'B'].includes(this.loadService.user.type)) {
-      this.checkRights();
-    } else {
-      this.getInformerShortData();
-    }
+    this.getInformerShortData();
   }
 
   private setData(type: TypeDataOfInformers) {
@@ -66,34 +60,6 @@ export class InformerComponent implements OnInit {
     this.dataInformer.button = `INFORMER.${type}.BUTTON`;
 
     this.statusInformer = type.toLowerCase() as TypeStatus;
-  }
-
-  private checkRights(): void {
-    const rights = this.informersService.checkRightsForLAndB();
-
-    if (rights) {
-      this.getInformerShortData();
-    } else {
-      if (this.loadService.user.type === 'L' && this.loadService.user.autorityId) {
-        this.profileService.getDelegatedRights().subscribe(
-          (data) => {
-            const rightsEnabled = data && data.authorities && data.authorities.some((elem) => {
-              return elem.mnemonic === 'INFORMER';
-            });
-            if (rightsEnabled) {
-              this.getInformerShortData();
-            } else {
-              this.setData('NO_RIGHTS');
-            }
-          },
-          () => {
-            this.setData('NO_RIGHTS');
-          }
-        );
-      } else {
-        this.setData('NO_RIGHTS');
-      }
-    }
   }
 
   private getWord(debtCount: Array<string>): string[] {
@@ -118,10 +84,11 @@ export class InformerComponent implements OnInit {
 
   private getTextToHint(code: string): void {
     if (this.informersService.hints[code].textWithDay) {
-      if (this.hintResponse.days == 1) {
-        this.hintText = "Остался последний день ";
+      if (+this.hintResponse.days === 1) {
+        this.hintText = 'Остался последний день ';
       } else {
-        this.hintText = this.informersService.getWord(this.hintResponse.days, "остался") + ' ' + this.hintResponse.days + ' ' + this.informersService.getWord(this.hintResponse.days, "день") + ' ';
+        this.hintText = this.declinePipe.transform(this.hintResponse.days, ['Остался', 'Осталось', 'Осталось'], false) +
+          ' ' + this.declinePipe.transform(this.hintResponse.days, ['день', 'дня', 'дней']) + ' ';
       }
       this.hintText += this.informersService.hints[code].textWithDay;
     } else {
@@ -136,15 +103,17 @@ export class InformerComponent implements OnInit {
 
         if (response.hint) {
           this.hintResponse = response.hint;
-          var hint = Object.keys(this.informersService.hints).find((code) => {
+          const hint = Object.keys(this.informersService.hints).find((code) => {
             return this.hintResponse.code === code;
           });
           this.getTextToHint(hint);
-        } else if (this.loadService.user.userType == 'P') {
-          this.getTextToHint("00");
+        } else if (this.loadService.user.userType === 'P') {
+          this.getTextToHint('00');
         }
 
-        if (response.result) {
+        if (response.error?.code === 50) {
+          this.setData('NO_RIGHTS');
+        } else if (response.result) {
           // есть начисления
           if (response.result.total) {
             const res = response.result;
