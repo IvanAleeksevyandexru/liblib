c47fae4de5a76c0ffd532e77b08c859451cc5b27
nikolay.filimonov <nikolay.filimonov@rtlabs.ru>
2021-04-13 20:46:16 +0300

    EPGUCORE-52301: Реализация класса Kid.

--- src/lib/models/kid.ts
+++ src/lib/models/kid.ts
@@ -1,4 +1,5 @@
-import { Document } from './document';
+import { CommonCert, Document, DocumentType } from './document';
+import { DatesHelperService } from '../services';
 
 export interface Kid {
   eTag?: string;
@@ -13,22 +14,108 @@ export interface Kid {
   middleName?: string;
   gender?: 'M' | 'F';
   birthDate?: string;
+  birthPlace?: string;
   inn?: string;
   snils?: string;
   documents?: {
     eTag?: string;
     stateFacts?: string[];
-    elements?: Document[];
-    docs?: Document[];
+    elements?: (Document | CommonCert)[];
+    docs?: (Document | CommonCert)[];
+    size?: number;
   };
+}
+
+export class Kid {
+  public id?: number;
+  public oid?: number;
+  public linkCode?: string;
+  public trusted?: boolean;
+  public firstName: string;
+  public lastName: string;
+  public middleName?: string;
+  public gender?: 'M' | 'F';
+  public birthDate?: string;
+  public birthPlace?: string;
+  public inn?: string;
+  public snils?: string;
   // кастомные поля
-  tempId?: number;
-  age?: number;
-  birthCert?: Document;
-  type?: string;
-  hasNewDoc?: boolean | string;
-  hasAccount?: boolean;
-  edit?: boolean;
-  state?: any;
-  statusMessage?: string;
+  public tempId?: number;
+  public age?: number;
+  public edit?: boolean;
+  public state?: any;
+  public birthCert?: Document | CommonCert;
+  public passport?: Document;
+  public cert?: CommonCert;
+  public documents?: {
+    eTag?: string;
+    stateFacts?: string[];
+    elements?: (Document | CommonCert)[];
+    docs?: (Document | CommonCert)[];
+    size?: number;
+  };
+  public hasAccount?: any; // TODO возможно, поле более не актуально
+  public readonly type;
+  private readonly fields = [
+    'id', 'oid', 'linkCode', 'firstName', 'lastName', 'middleName',
+    'gender', 'birthDate', 'birthPlace', 'trusted', 'inn', 'snils'
+  ];
+
+  constructor(child: Kid, types: {[key: string]: DocumentType}) {
+    this.mapFields(this, child);
+
+    this.type = types.CHILD;
+    this.age = DatesHelperService.calcAge(this.birthDate);
+
+    if (this.age < 18) {
+      this.state = 'subscription';
+    }
+
+    this.documents = {
+      elements: child.documents.elements
+    };
+    this.birthCert = { type: types.BIRTH_CERTIFICATE_RF } as Document;
+    this.passport = { type: types.PASSPORT } as Document;
+    this.cert = { type: types.FATHERHOOD_CERT } as CommonCert;
+
+    this.documents.elements.forEach(doc => {
+      ['eTag', 'stateFacts'].forEach(field => delete doc[field]);
+
+      switch (doc.type) {
+        case types.BIRTH_CERTIFICATE:
+        case types.BIRTH_CERTIFICATE_RF:
+        case types.BIRTH_CERTIFICATE_FID:
+        case types.BIRTH_CERTIFICATE_OLD:
+          this.birthCert = doc;
+          break;
+        case types.PASSPORT:
+          this.passport = doc as Document;
+          break;
+        case types.FATHERHOOD_CERT:
+          this.cert = doc as CommonCert;
+          break;
+      }
+    });
+  }
+
+  private mapFields(target, origin) {
+    Object.keys(origin).forEach(field => {
+      if (this.fields.includes(field)) {
+        target[field] = origin[field];
+      }
+    });
+  }
+
+  public processChild(): Kid {
+    const child = {};
+
+    this.mapFields(child, this);
+
+    return {
+      ...child,
+      documents: {
+        elements: this.documents.elements
+      }
+    } as Kid;
+  }
 }
