97938e2409ab4339f52461d16f150e7b9ed9c2bb
anna.ryazhskikh@rtlabs.ru <anna.ryazhskikh@rtlabs.ru>
2021-04-15 12:36:40 +0300

    EPGUCORE-53274 Изменить переход с ленты уведомлений

--- src/lib/services/feeds/feeds.service.ts
+++ src/lib/services/feeds/feeds.service.ts
@@ -189,7 +189,7 @@ export class FeedsService {
         url += `draft/${feed.extId}`;
         break;
       case 'PAYMENT':
-        url += `payment/detail/paying/${feed.extId}`;
+        url = `${this.loadService.config.paymentUrl}/pay/details/${feed.extId}`;
         break;
       case 'EQUEUE':
         if (feed.data && feed.data.parentOrderId) {
