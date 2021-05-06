52bf32502e0faf9e174f3ad61a923eb1d3f6c267
mbuntin <maxim.buntin@rtlabs.ru>
2021-05-06 22:09:27 +0300

    EPGUCORE-50244: added check methods for uploader

--- src/lib/components/file-uploader/file-uploader.component.ts
+++ src/lib/components/file-uploader/file-uploader.component.ts
@@ -23,12 +23,21 @@ export class FileUploaderComponent implements OnInit, ControlValueAccessor, Vali
   @Input() public formControlName?: string;
   @Input() public multiple?: boolean;
   @Input() public invalid = false;
+  // типы файлов строка через , без пробелов
+  // png,jpg
+  @Input() public fileTypes = '';
+  @Input() public maxLength = 0;
+  // в байтах максимальный размер
+  // 5Mb = 10485760 = 5 * 1024 * 1024
+  @Input() public maxFileSize = 0;
   @Input() public validationShowOn: ValidationShowOn | string | boolean | any = ValidationShowOn.TOUCHED;
 
   public invalidDisplayed: boolean;
   public onChange: (FileList) => void;
   public files: File[] | null = null;
   public touched = false;
+  public fileTypesArray: string[];
+  public errorMessage = '';
 
 
   @HostListener('change', ['$event.target.files']) public emitFiles( event: FileList ) {
@@ -36,10 +45,17 @@ export class FileUploaderComponent implements OnInit, ControlValueAccessor, Vali
     if (!this.files) {
       this.files = [];
     }
-
+    this.errorMessage = '';
+    console.log('$event.target.files', event);
+    const maxFilesSize = this.checkMaxFilesSize(event);
+    const filesLength = this.checkFilesLength(event.length);
+    if (!maxFilesSize || !filesLength) {
+      return false;
+    }
     for (let i = 0; i < event.length; i++) {
       const existingFile = this.findFile(event[i]);
-      if (!existingFile) {
+      const checkType = this.checkFileTypes(event[i]);
+      if (!existingFile && checkType) {
         this.files.push(event[i]);
       }
     }
@@ -54,6 +70,9 @@ export class FileUploaderComponent implements OnInit, ControlValueAccessor, Vali
 
   public ngOnInit() {
     this.control = this.controlContainer && this.formControlName ? this.controlContainer.control.get(this.formControlName) : null;
+    if (this.fileTypes) {
+      this.fileTypesArray = this.fileTypes.split(',');
+    }
   }
 
   private findFile(file): File {
@@ -103,6 +122,45 @@ export class FileUploaderComponent implements OnInit, ControlValueAccessor, Vali
       return 'err';
     }
     return ext;
+  }
+
+  public checkFileTypes(file) {
+    let result = true;
+    const nameArr = file.name.split('.');
+    const fileType = nameArr[nameArr.length - 1];
+    if (this.fileTypes) {
+      if (this.fileTypesArray.indexOf(fileType) < 0) {
+        this.errorMessage = `Расширение загружаемого файла не входит в список разрешенных: ${file.name}`;
+        result = false;
+      }
+    }
+    return result;
+  }
+
+  public checkFilesLength(length) {
+    if (this.maxLength) {
+      return length <= this.maxLength;
+    }
+    return true;
+  }
+
+  public checkMaxFilesSize(files: FileList) {
+    if (this.maxFileSize && files && files.length) {
+      const filesToArray = Array.from(files);
+      const currentSize = this.files.reduce((sum, file) => {
+        const fileSize = isNaN(file.size) ? 0 : file.size;
+        return sum + fileSize;
+      }, 0);
+      const fullSize = filesToArray.reduce((sum, file) => {
+        const fileSize = isNaN(file.size) ? 0 : file.size;
+        return sum + fileSize;
+      }, 0);
+      if (currentSize + fullSize > this.maxFileSize) {
+        this.errorMessage = `Максимальный размер файлов превышает ${this.sizeInMB(this.maxFileSize)}`;
+        return false;
+      }
+    }
+    return true;
   }
 
   public remove(index: number): void {
