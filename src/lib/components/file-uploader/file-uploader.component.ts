fe43dc036679d18949af2bbbf9810d1cb1c39ac0
dmitriy.makarov <dmitry.makarov@rtlabs.ru>
2021-05-12 17:42:13 +0300

    EPGUCORE-50244 (EPGUCORE-56011):  Доработка компонента lib-fileuploader

--- src/lib/components/file-uploader/file-uploader.component.ts
+++ src/lib/components/file-uploader/file-uploader.component.ts
@@ -1,20 +1,57 @@
-import { Component, DoCheck, ElementRef, Host, HostListener, Input, OnInit, Optional, SkipSelf } from '@angular/core';
-import { AbstractControl, ControlContainer, ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  DoCheck,
+  ElementRef,
+  forwardRef,
+  Host,
+  HostListener,
+  Input,
+  OnInit,
+  Optional,
+  SkipSelf, ViewChild
+} from '@angular/core';
+import {
+  AbstractControl,
+  ControlContainer,
+  ControlValueAccessor,
+  NG_VALIDATORS,
+  NG_VALUE_ACCESSOR,
+  ValidationErrors
+} from '@angular/forms';
 import { ValidationHelper } from '../../services/validation-helper/validation.helper';
 import { Validated, ValidationShowOn } from '../../models/validation-show';
+import { FileUploaderService } from '../../services/file-uploader/file-uploader.service';
 
-const FILE_TYPES = ['png', 'jpg', 'bmp', 'tif', 'gif', 'key', 'ppt', 'pdf', 'rar', 'zip', 'doc', 'txt', 'rtf', 'flv', 'mov', 'mpg', 'sig', 'xml'];
+const FILE_TYPES = ['png', 'jpg', 'jpeg', 'bmp', 'tif', 'gif', 'key', 'ppt', 'pdf', 'rar', 'zip', 'doc', 'txt', 'rtf', 'flv', 'mov', 'mpg', 'sig', 'xml'];
+
+export interface FileUpload {
+  size: number;
+  type: string;
+  lastModified: number;
+  name: string;
+  error: string;
+  mnemonic: string;
+  uploadInProcess: boolean;
+}
 
 @Component({
   selector: 'lib-file-uploader',
   templateUrl: './file-uploader.component.html',
   styleUrls: ['./file-uploader.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [
     {
       provide: NG_VALUE_ACCESSOR,
       useExisting: FileUploaderComponent,
       multi: true
-    }
+    },
+    {
+      provide: NG_VALIDATORS,
+      useExisting: forwardRef(() => FileUploaderComponent),
+      multi: true,
+    },
   ]
 })
 export class FileUploaderComponent implements OnInit, ControlValueAccessor, Validated, DoCheck {
@@ -30,14 +67,19 @@ export class FileUploaderComponent implements OnInit, ControlValueAccessor, Vali
   // в байтах максимальный размер
   // 5Mb = 10485760 = 5 * 1024 * 1024
   @Input() public maxFileSize?: number;
+  @Input() public orderId?: number;
   @Input() public validationShowOn: ValidationShowOn | string | boolean | any = ValidationShowOn.TOUCHED;
+  @Input() public storageServiceUrl = '';
+
+  @ViewChild('fileInput', {static: false}) public fileInput: ElementRef;
 
   public invalidDisplayed: boolean;
   public onChange: (FileList) => void;
-  public files: File[] | null = null;
+  public files: FileUpload[] | null = null;
   public touched = false;
   public fileTypesArray: string[];
   public errorMessage = '';
+  private commit(value: any) {}
 
 
   @HostListener('change', ['$event.target.files']) public emitFiles( event: FileList ) {
@@ -46,9 +88,10 @@ export class FileUploaderComponent implements OnInit, ControlValueAccessor, Vali
       this.files = [];
     }
     this.errorMessage = '';
-    console.log('$event.target.files', event);
     const maxFilesSize = this.checkMaxFilesSize(event);
     const filesLength = this.checkFilesLength(event.length);
+    const needUploadToServer = this.storageServiceUrl && !!this.orderId;
+
     if (!maxFilesSize || !filesLength) {
       return false;
     }
@@ -56,16 +99,59 @@ export class FileUploaderComponent implements OnInit, ControlValueAccessor, Vali
       const existingFile = this.findFile(event[i]);
       const checkType = this.checkFileTypes(event[i]);
       if (!existingFile && checkType) {
-        this.files.push(event[i]);
+        const file: FileUpload = {
+          mnemonic: btoa(event[i].name),
+          uploadInProcess: needUploadToServer,
+          name: event[i].name,
+          size: event[i].size,
+          type: event[i].type,
+          error: '',
+          lastModified: event[i].lastModified
+        };
+        this.files.push(file);
+
+        if (needUploadToServer) {
+          const formData = new FormData();
+          formData.append('file', event[i]);
+          formData.append('name', file.name);
+          if (this.orderId) {
+            formData.append('objectType', '2');
+            formData.append('objectId', this.orderId.toString());
+          }
+          formData.append('mnemonic', btoa(file.name));
+          this.saveFileToServer(formData, this.files[this.files.length - 1]);
+        }
       }
     }
 
-    this.onChange(this.files);
-    this.check();
+    if (!needUploadToServer) {
+      this.commit(this.files);
+      this.check();
+    }
   }
 
   constructor(private host: ElementRef<HTMLInputElement>,
+              private fileUploaderService: FileUploaderService,
+              private cd: ChangeDetectorRef,
               @Optional() @Host() @SkipSelf() private controlContainer: ControlContainer) {
+  }
+
+  private saveFileToServer(formData: FormData, file: FileUpload): void {
+    const process =  (file: FileUpload, withError: boolean) => {
+      if (withError) {
+        file.error = 'Во время загрузки возникла ошибка';
+      }
+      file.uploadInProcess = false;
+      this.commit(this.files);
+      this.check();
+      this.cd.detectChanges();
+    }
+
+    this.fileUploaderService.saveFileToStorage(this.storageServiceUrl, formData).subscribe(res => {
+      process(file, false);
+    }, err => {
+      process(file, true);
+    });
   }
 
   public ngOnInit() {
@@ -75,7 +161,7 @@ export class FileUploaderComponent implements OnInit, ControlValueAccessor, Vali
     }
   }
 
-  private findFile(file): File {
+  private findFile(file): FileUpload {
     return this.files.find(existingFile => {
       return (
         existingFile.name         === file.name &&
@@ -86,14 +172,17 @@ export class FileUploaderComponent implements OnInit, ControlValueAccessor, Vali
     });
   }
 
-  public writeValue( value: null ) {
-    // clear file input
+  public writeValue( value: null | [] ) {
     this.host.nativeElement.value = '';
-    this.files = null;
+    if (value?.length) {
+      this.files = value;
+    } else {
+      this.files = null;
+    }
   }
 
   public registerOnChange( fn: () => void ) {
-    this.onChange = fn;
+    this.commit = fn;
   }
 
   public registerOnTouched( fn: () => void ) {
@@ -164,11 +253,38 @@ export class FileUploaderComponent implements OnInit, ControlValueAccessor, Vali
   }
 
   public remove(index: number): void {
-    this.touched = true;
-    this.files.splice(index, 1);
+    const deleteProcess = (index: number, deletedFile: FileUpload, withError: boolean) => {
+      if (withError) {
+        deletedFile.error = 'Во время удаления возникла ошибка';
+      }
+      deletedFile.uploadInProcess = false;
+      this.files.splice(index, 1);
+      this.commit(this.files);
+      this.check();
+      this.cd.detectChanges();
+    }
 
-    this.onChange(this.files);
-    this.check();
+    this.fileInput.nativeElement.value = '';
+    this.touched = true;
+    const deletedFile = this.files[index];
+    const objectType = '2';
+    if (this.storageServiceUrl && this.orderId && !deletedFile.error) {
+      deletedFile.uploadInProcess = true;
+      this.fileUploaderService.deleteFileFromStorage(this.storageServiceUrl, this.orderId.toString(), objectType, deletedFile.mnemonic).subscribe(res => {
+        deleteProcess(index, deletedFile, false);
+      }, err => {
+        deleteProcess(index, deletedFile, true);
+      })
+    } else {
+      deleteProcess(index, deletedFile, false);
+    }
+  }
+
+  public validate(control: AbstractControl): ValidationErrors | null {
+    if (this.errorMessage || this.files?.some(file => file.error)) {
+      return {fileUploadInvalid: true};
+    }
+    return null;
   }
 
   public check() {
