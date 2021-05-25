2fac3e85f8325f7f8eccb634df3f01a5744fd229
Dovbysh Oleg <oleg.dovbysh@rtlabs.ru>
2021-05-25 15:15:40 +0300

    EPGUCORE-56943 [front] Доработка FileUploader, добавление подписи с ограничениями

--- src/lib/components/file-uploader/file-uploader.component.ts
+++ src/lib/components/file-uploader/file-uploader.component.ts
@@ -121,7 +121,7 @@ export class FileUploaderComponent implements OnInit, ControlValueAccessor, Vali
             formData.append('objectType', '2');
             formData.append('objectId', this.orderId.toString());
           }
-          formData.append('mnemonic', btoa(file.name));
+          formData.append('mnemonic', btoa(name));
           this.saveFileToServer(formData, this.files[this.files.length - 1]);
         }
       }
