9b07d6a0944c359630a0573ed896e1fff68beb14
Konstantin Skok <skok.konstantin@gmail.com>
2021-07-16 13:24:45 +0300

    EPGU-62161 Изменения в меню LK_SETTINGS_SIDE_TABS

--- src/lib/services/constants.service.ts
+++ src/lib/services/constants.service.ts
@@ -125,6 +125,13 @@ export class ConstantsService {
       url: '/messages',
       metric: { name: this.TABS_METRIC_NAME, action: 'messages' },
     },
+    {
+      id: 'permissions',
+      name: 'TABS.PERMISSIONS.TITLE',
+      url: '/permissions',
+      metric: { name: this.TABS_METRIC_NAME, action: 'permissions' },
+      trusted: true,
+    },
   ]);
   public readonly LK_DEPT_TABS = new Tabs([
     {
@@ -390,6 +397,12 @@ export class ConstantsService {
       id: 'statements',
       name: 'PROFILE.TABS.STATEMENTS',
       url: '/profile/statements',
+      access: ['AL20'],
+    },
+    {
+      id: 'empowerments',
+      name: 'PROFILE.TABS.EMPOWERMENTS',
+      url: '/profile/empowerments',
       access: ['AL20'],
     },
     {
