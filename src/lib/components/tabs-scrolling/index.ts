97146c5737fba2479f318d341237ebd68ccc6d35
Dovbysh Oleg <oleg.dovbysh@rtlabs.ru>
2021-08-05 16:11:48 +0300

    move tabs-scrolling

--- src/lib/components/tabs-scrolling/index.ts
+++ src/lib/components/tabs-scrolling/index.ts
@@ -1,1 +1,1 @@
-export * from './public_api';
+export * from '../../tabs-scrolling-public_api';
---
+++ src/lib/tabs-scrolling-public_api.ts
@@ -0,0 +1,2 @@
+export * from './components/tabs-scrolling/tabs-scrolling.component';
+export * from './components/tabs-scrolling/tabs-scrolling.module';
