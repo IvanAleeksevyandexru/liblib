b06cf02b3b5744aec6193c956316e6722b933a1f
IKryazhev <IKryazhev@stream.ru>
2021-08-10 17:20:10 +0300

    EPGUCORE-65653: Загрузчик, разделение типов файлов

--- src/lib/epgu-lib.module.ts
+++ src/lib/epgu-lib.module.ts
@@ -160,6 +160,7 @@ import { RegionCasesComponent } from './components/region-cases/region-cases.com
 import { FileUploaderComponent } from './components/file-uploader/file-uploader.component';
 import { ReversePipe } from './pipes/reverse/reverse.pipe';
 import { FileExtensionIconComponent } from './components/file-extension-icon/file-extension-icon.component';
+import { TrimFileTypesPipe } from './pipes/trim-file-types/trim-file-types.pipe';
 
 
 registerLocaleData(localeRu, 'ru');
@@ -310,6 +311,7 @@ registerLocaleData(localeRu, 'ru');
     FileUploaderComponent,
     ReversePipe,
     FileExtensionIconComponent,
+    TrimFileTypesPipe
   ],
   imports: [
     CommonModule,
