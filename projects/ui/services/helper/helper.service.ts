7c7ef2000bd573b54f2ed20cf8a9f0bf57e76b35
Konoplyannikov Dmitry <dkonoplyannikov@it-one.ru>
2021-11-09 21:57:56 +0500

    EPGUCORE-76100: добавлен метод сравнения объектов

--- projects/ui/services/helper/helper.service.ts
+++ projects/ui/services/helper/helper.service.ts
@@ -611,4 +611,30 @@ export class HelperService {
     this.reloadAbsoluteInternalLinks = value;
   }
 
+    /**
+   * Проверка на идентичность объектов.
+   */
+     public static deepEqual(object1: any, object2: any): boolean {
+      if (object1 == null || object2 == null) {
+        return object2 === object1;
+      }
+      const keys1 = Object.keys(object1);
+      const keys2 = Object.keys(object2);
+      if (keys1.length !== keys2.length) {
+        return false;
+      }
+      for (const key of keys1) {
+        const val1 = object1[key];
+        const val2 = object2[key];
+        const areObjects = HelperService.isObject(val1) && HelperService.isObject(val2);
+        if (
+          (areObjects && !HelperService.deepEqual(val1, val2)) ||
+          (!areObjects && val1 !== val2)
+        ) {
+          return false;
+        }
+      }
+      return true;
+    }
+
 }
