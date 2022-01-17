c51ceabbd55a4c45881153cfa8aca2e382b89334
dmitriy.makarov <dmitry.makarov@rtlabs.ru>
2022-01-17 14:18:08 +0300

    EPGUCORE-84511: Измерять клики на ссылки в футере в яндекс метрике

--- projects/ui/components/small-footer/small-footer.component.ts
+++ projects/ui/components/small-footer/small-footer.component.ts
@@ -1,7 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { LoadService } from '@epgu/ui/services/load';
 import { FooterService } from '@epgu/ui/services/footer';
-import { MainFooter } from '@epgu/ui/models';
+import { MainFooter, MainFooterBlockLink } from '@epgu/ui/models';
+import { YaMetricService } from '@epgu/ui/services/ya-metric';
 
 @Component({
   selector: 'lib-small-footer',
@@ -17,10 +18,26 @@ export class SmallFooterComponent implements OnInit {
 
   constructor(
     public loadService: LoadService,
-    public footerService: FooterService
+    public footerService: FooterService,
+    public yaMetric: YaMetricService
   ) {
   }
 
   public ngOnInit(): void {
+  }
+
+  public openLink(event: Event, link: MainFooterBlockLink): void {
+    event.preventDefault();
+    this.sendMetric(link);
+  }
+
+  public sendMetric(link: MainFooterBlockLink): void {
+    this.yaMetric.callReachGoal('footer', ['footer', link.title], () => {
+      if (link.target === '_blank') {
+        window.open(link.url, '_blank', 'noopener noreferrer');
+      } else {
+        location.href = link.url;
+      }
+    });
   }
 }
